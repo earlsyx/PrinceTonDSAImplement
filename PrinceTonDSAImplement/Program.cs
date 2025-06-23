4b03403b625669a664e7fec91e8b8b190b53311b
Earl Syx Hernandez <hernandezearlsyxmail@gmail.com>
2025-06-24 06:34:55 +0800

    Add project files.

---
+++ PrinceTonDSAImplement/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        var uf = new QuickFindUF(10); // This should work now!
+
+        uf.Union(4, 3);
+        uf.Union(3, 8);
+        uf.Union(6, 5);
+        uf.Union(9, 4);
+        uf.Union(2, 1);
+
+        Console.WriteLine(uf.Connected(8, 9)); // True
+        Console.WriteLine(uf.Connected(5, 4)); // False
+
+        uf.Print();
+
+        Console.ReadLine(); // Pause the console
+    }
+}
---
+++ PrinceTonDSAImplement/QuickFindUF.cs
@@ -0,0 +1,33 @@
+﻿// No namespace needed if in same folder
+public class QuickFindUF
+{
+    private int[] id;
+
+    public QuickFindUF(int n)
+    {
+        id = new int[n];
+        for (int i = 0; i < n; i++)
+            id[i] = i;
+    }
+
+    public bool Connected(int p, int q)
+    {
+        return id[p] == id[q];
+    }
+
+    public void Union(int p, int q)
+    {
+        int pid = id[p];
+        int qid = id[q];
+        for (int i = 0; i < id.Length; i++)
+        {
+            if (id[i] == pid)
+                id[i] = qid;
+        }
+    }
+
+    public void Print()
+    {
+        Console.WriteLine("Current state: " + string.Join(", ", id));
+    }
+}
