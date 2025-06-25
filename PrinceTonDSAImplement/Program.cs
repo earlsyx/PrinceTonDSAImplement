bfa1f23ebe6b2e5cd5fc5dc13e93b5eb8944a2cc
Earl Syx Hernandez <hernandezearlsyxmail@gmail.com>
2025-06-26 06:13:46 +0800

    3 Sum Brute force algo sample

--- PrinceTonDSAImplement/Program.cs
+++ PrinceTonDSAImplement/Program.cs
@@ -1,5 +1,5 @@
 ﻿using System;
-
+using PrinceTonDSAImplement;
 class Program
 {
     static void Main()
@@ -7,18 +7,26 @@ class Program
         //var uf = new QuickFindUF(10); // n*2
         //var uf = new QuickUnionUF(10); // n
         //var uf = new WeightedQuickUnionUF(10); // log n
-        var uf = new WeightedQuickUnionWithPathCompressionUF(10); // mear constant log n
+        //var uf = new WeightedQuickUnionWithPathCompressionUF(10); // mear constant log n
 
-        uf.Union(4, 3);
-        uf.Union(3, 8);
-        uf.Union(6, 5);
-        uf.Union(9, 4);
-        uf.Union(2, 1);
+        //uf.Union(4, 3);
+        //uf.Union(3, 8);
+        //uf.Union(6, 5);
+        //uf.Union(9, 4);
+        //uf.Union(2, 1);
 
-        Console.WriteLine(uf.Connected(8, 9)); // True
-        Console.WriteLine(uf.Connected(5, 4)); // False
+        //Console.WriteLine(uf.Connected(8, 9)); // True
+        //Console.WriteLine(uf.Connected(5, 4)); // False
 
-        uf.Print();
+        //uf.Print();
+
+        // --- 3 sum bruite force
+        int[] numbers = { -2, -1, 0, 1, 2, 3 };
+        var threeSum = new ThreeSumBruitForce();
+        int result = threeSum.Count(numbers);
+
+        Console.WriteLine($"Number of triplet that sum to 0: {result}");
+        
 
         Console.ReadLine(); // Pause the console
     }
---
+++ PrinceTonDSAImplement/ThreeSumBruitForce.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PrinceTonDSAImplement
+{
+    public class ThreeSumBruitForce
+    {
+        public int Count(int[] a)
+        {
+            int n = a.Length;
+            int count = 0;
+            for (int i = 0; i < n; i++)
+                for (int j = i + 1; j < n; j++) 
+                    for (int k = j + 1; k < n; k++)
+                      if (a[i] + a[j] + a[k] == 0)        
+                            count++;
+                       
+            return count;
+        }
+    }
+}
